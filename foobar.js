eef23acd93cff08540948fdfc6a7a5bd2368a570
valnicky <valery0007@gmail.com>
2021-11-05 23:29:37 +0100

    styles

--- foobar.js
+++ foobar.js
@@ -1,3 +1,12 @@
 //api_key : feb0a933b10a99bd61842a37d63a0f90
 // https://api.themoviedb.org/3/movie/550?api_key=feb0a933b10a99bd61842a37d63a0f90
 // https://netflix-clone-7cdf7.web.app
+/*  "scripts": {
+        
+        "start": "react-scripts start",
+        "predeploy": "npm run build",
+        "deploy": "gh-pages -d build",
+        "build": "react-scripts build",
+        "test": "react-scripts test",
+        "eject": "react-scripts eject"
+    }, */
--- src/Row.js
+++ src/Row.js
@@ -23,7 +23,7 @@ function Row({title,  fetchUrl}) {
             <h2>{title}</h2>
             <div className="row__posters">
                     {movies.map((movie)=> (
-                    <img className="row__poster" src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+                    <img key={movie.id} className="row__poster" src={`${base_url}${movie.poster_path}`} alt={movie.name} />
             ))}
             </div>
      
