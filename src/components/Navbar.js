fdb502519b4de57fc8b73cd16ff6e484eb55e5ad
valnicky <valery0007@gmail.com>
2021-11-08 23:48:29 +0100

    video

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import '../Navbar.css';
 
 function Navbar() {
+    const [show, handleShow] = useState(false);
+    
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if(window.scrollY > 100) {
+                handleShow(true);
+            } else {
+                handleShow(false);
+            }
+        });
+        return ( ) => {
+            window.removeEventListener('scroll');
+        }
+    }, []);
+    
     return (
-        <div className= "navbar">
+        <div className= {`navbar ${show && "navbar__black"}`}>
             <img className="navbar__logo" src="../img/netflix.png" alt="Netflix Logo" />
             <img src="../img/avatar.png" alt="Netflix Avatar" className="navbar__avatar" />
         </div>
